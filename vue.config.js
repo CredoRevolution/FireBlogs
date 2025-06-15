726f8038a8c25879af8c32b17412b2acbbb91a67
Credo Revolution <sadistik871@mail.ru>
2025-06-16 00:27:14 +0300

    Configure for GitHub Pages deployment

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production' ? '/FireBlogs/' : '/',
   transpileDependencies: true,
   chainWebpack: (config) => {
       const svgRule = config.module.rule('svg')
